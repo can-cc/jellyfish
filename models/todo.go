dd0bdf2847438ec3d94704bc57b83daeae16a9be
fwchen <crystal.tyan@gmail.com>
2018-05-22 21:53:37 +0800

    update

--- models/todo.go
+++ models/todo.go
@@ -41,8 +41,8 @@ func GetTodosFromDB(db *sql.DB, userId string) TodoCollection {
 		var deadline time.Time
 		var createdAt time.Time
 		err2 := rows.Scan(&todo.ID, &todo.Content, &todo.Detail, &deadline, &todo.Status, &todo.Done, &createdAt)
-		todo.Deadline = deadline.UnixNano()
-		todo.CreatedAt = createdAt.UnixNano()
+		todo.Deadline = deadline.UnixNano() / int64(time.Millisecond)
+		todo.CreatedAt = createdAt.UnixNano() / int64(time.Millisecond)
 
 		if err2 != nil {
 			panic(err2)
@@ -73,7 +73,7 @@ func UpdateTodo(db *sql.DB, todo *Todo) (int64, error) {
 
 	defer stmt.Close()
 
-	result, err2 := stmt.Exec(todo.Content, todo.Detail, todo.Done, todo.Deadline, todo.Status, time.Now().UnixNano(), todo.ID)
+	result, err2 := stmt.Exec(todo.Content, todo.Detail, todo.Done, todo.Deadline, todo.Status, time.Now().UnixNano()/int64(time.Millisecond), todo.ID)
 
 	if err2 != nil {
 		panic(err2)
@@ -95,7 +95,7 @@ func CreateTodo(db *sql.DB, todo *Todo) (int64, error) {
 	defer stmt.Close()
 
 	// Replace the '?' in our prepared statement with 'name'
-	result, err2 := stmt.Exec(todo.Content, todo.Detail, todo.CreaterId, todo.Deadline, todo.Status, time.Now().UnixNano())
+	result, err2 := stmt.Exec(todo.Content, todo.Detail, todo.CreaterId, todo.Deadline, todo.Status, time.Now().UnixNano()/int64(time.Millisecond))
 
 	// Exit if we get an error
 	if err2 != nil {
