b145434da7a62039da2f094a09ed5c60f0d35461
fwchen <me@chenfangwei.com>
2020-05-24 12:11:06 +0800

    update

--- domain/taco_box/command/command.go
+++ domain/taco_box/command/command.go
@@ -2,11 +2,11 @@ package command
 
 type CreateTacoBoxCommand struct {
 	CreatorId string
-	Name      string `json:"name"`
+	Name      string `json:"name" validate:"required"`
 }
 
 type UpdateTacoCommand struct {
 	Name            string `json:"name"`
 	OperationUserID string
-	TacoBoxID       string
+	TacoBoxID       string `validate:"required"`
 }
